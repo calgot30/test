2a65dede7b5ddbc33ff5d634a46ecb663b2b1435
Callum <callumgott@gmail.comgit config --global user.name Callum>
2019-07-31 09:04:06 +1200

    blah

---
+++ array_exercise7.php
@@ -0,0 +1,17 @@
+<?php
+
+$color = array("red", "blue", "green", "gold", "yellow");
+$names = array("mikaere", "lisa", "shona", "willie", "rachel");
+
+
+echo"<ol>";
+for($i =0; $i < 5; $i++)
+{
+    echo "<li style=\"color: ".$color[$i]."\">".$names[$i]."</li>";
+}
+echo"</ol>";
+?>
+
+
+
+
